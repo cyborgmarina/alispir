f13edc9ba06c5b86a5a4b89453065de0c791158f
André Cabrera <andrecabrera@alcipir.local>
2016-02-03 15:59:06 -0600

    Change operators, add comments

--- alispir.c
+++ alispir.c
@@ -1,51 +1,54 @@
+// Compiler flags: -std=c99 -Wall alispir.c mpc.c -ledit -lm -o alispir
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #include <editline/readline.h>
-#include <editline/history.h>
 
 #include "mpc.h"
 
 int main(int argc, char** argv) {
 
-mpc_parser_t* Number = mpc_new("number");
-mpc_parser_t* Operator = mpc_new("operator");
-mpc_parser_t* Expr = mpc_new("expr");
-mpc_parser_t* Lispy = mpc_new("lispy");
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t* Lispy = mpc_new("lispy");
 
-mpca_lang(MPCA_LANG_DEFAULT,
-  "                                                                 \
-    number   : /-?[0-9]+/ ;                             	    \
-    operator : '+' | '-' | '*' | '/' | '%' | '^' ;      \
-    expr     : <number> | '(' <operator> <expr>+ ')' ;              \
-    lispy    : /^/ <expr>+ /$/ ;                         \
-  ",
-  Number, Operator, Expr, Lispy);
+  // Grammar
+  // e.g. Alispir expression: (+ 1 2)
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                                 \
+      number   : /-?[0-9]+/ ;                             	    \
+      operator : /add/ | /sub/ | /mul/ | /div/ | /mod/ | /pow/;      \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;              \
+      lispy    : /^/ <expr>+ /$/ ;                         \
+    ",
+    Number, Operator, Expr, Lispy);
 
-// e.g. Alispir expression: (+ 1 2)
-puts("Alispir Version 0.1");
-puts("Press Ctrl+C to Exit");
+  puts("Alispir Version 0.1");
+  puts("Press Ctrl+C to Exit");
 
-while(1) {
+  // REPL
+  while(1) {
 
-char* input = readline("alispir> ");
+    char* input = readline("alispir> ");
 
-add_history(input);
+    add_history(input);
 
-mpc_result_t r;
-if (mpc_parse("<stdin>", input, Lispy, &r)) {
-	mpc_ast_print(r.output);
-	mpc_ast_delete(r.output);
-} else {
-	mpc_err_print(r.error);
-	mpc_err_delete(r.error);
-}
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+    	mpc_ast_print(r.output);
+    	mpc_ast_delete(r.output);
+    } else {
+    	mpc_err_print(r.error);
+    	mpc_err_delete(r.error);
+    }
 
-free(input);
+    free(input);
 
-}
+    }
 
-mpc_cleanup(4, Number, Operator, Expr, Lispy);
-return 0;
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
+    return 0;
 
 }
